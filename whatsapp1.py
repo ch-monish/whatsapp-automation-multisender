4af103e8af3ded475da6e7f743ea41ce0cfa757c
ch-monish <50499828+ch-monish@users.noreply.github.com>
2020-01-24 19:59:27 +0530

    Update whatsapp1.py

--- whatsapp1.py
+++ whatsapp1.py
@@ -1,18 +1,19 @@
 from selenium import webdriver
 import time
+message=input("ENTER THE MESSAGE YOU WANT TO SEND\n")
 driver=webdriver.Chrome("C:/Users/this pc/Desktop/chromewebdriver/chromedriver.exe") 
 driver.get('https://web.whatsapp.com/')
 time.sleep(30)
 
 
 
-
-contacts=['monish','sudeep','akhiless','haarika','howle','chakra']
+#ENTER YOUR CONTACTS TO SENT
+contacts=['a','b','c','d','e','f']
 for i in contacts:
 	driver.find_element_by_class_name('eiCXe').send_keys(i)
 	driver.find_element_by_class_name('X7YrQ').click()
 	time.sleep(5)
-	driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[2]/div/div[2]").send_keys('hello')
+	driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[2]/div/div[2]").send_keys(message)
 	driver.find_element_by_class_name('_3M-N-').click()
 	time.sleep(5)
 driver.close()
